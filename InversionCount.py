36fdea55713e6310960ba3625b8524552981d713
jrgustmadtown <jrgust@wisc.edu>
2025-10-08 10:56:04 -0500

    solve one

--- InversionCount.py
+++ InversionCount.py
@@ -1,20 +1,42 @@
 import sys
-import heapq
 
 inputy = sys.stdin.read().splitlines()
 k = int(inputy[0])
 index = 1
 outputy = []
 
-def IC(size, unsorted):
+def MergeCount(front, back):
+    S = []
+    c=0
+    while front or back:
+        if not back:
+            S.append(front.pop(0))
+        elif not front:
+            S.append(back.pop(0))
+        else:
+            if front.peek(0) < back.peek(0):
+                S.append(front.pop(0))
+            else:
+                S.append(back.pop(0))
+                c+=len(front)
+    return S,c
 
+
+
+def CountSort(size, elems):
+    if size <= 1:
+        return 1
+    mid = size // 2
+    front, fc = CountSort(mid, elems[:mid])
+    back, bc = CountSort(size-mid, elems[mid:])
+    merged, c = MergeCount(front, back)
 
 for _ in range(k):
     j = int(inputy[index])
     index+=1
     unsorted = list(map(int, inputy[index].split()))
     index+=1
-    count = IC(j,unsorted)
+    sorted, count = CountSort(j,unsorted)
     outputy.append(count)
 
 for _ in outputy:
